0fac17165de8a3796ef076962b0fa032e2448f96
Karthikamn <69958553+Karthikamn@users.noreply.github.com>
2025-08-06 16:43:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("New line added for diff");
-        System.out.println("New line added");
+        System.out.println("New changes");
     }
 }
